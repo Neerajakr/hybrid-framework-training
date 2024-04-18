f20287ce68ccf4a926a5d75951653ab25890c050
Neerajakr <kr.neeraja25@gmail.com>
2024-04-17 23:34:07 -0700

    upload file test

--- src/test/java/com/allianz/test/AddEmployeeTest.java
+++ src/test/java/com/allianz/test/AddEmployeeTest.java
@@ -31,20 +31,6 @@ public class AddEmployeeTest extends AutomationWrapper{
 	    System.out.println("Login: "+error);
 	    Assert.assertEquals(error,"File type not allowed");
 	    Assert.assertTrue(error.contains("File type not allowed"));
-	    
-	    
-	  
-//	    WebElement fileInput = driver.findElement(By.xpath("(//button[@type='button'])[3]"));
-//	    
-//	    fileInput.sendKeys(uploadFile.getAbsolutePath());
-//	    fileInput.sendKeys("C:\\Users\\Administrator\\Desktop\\SamplePDF.pdf");
-//	    driver.findElement(By.id("file-submit")).click();
-//	    
-//	    String error =driver.findElement(By.xpath("//span[text()='File type not allowed']")).getText();
-//	    System.out.println("Login: "+error);
-//	    Assert.assertEquals(error,"File type not allowed");
-	    
-	    
 	    System.out.println("End");
 	   
 	    
