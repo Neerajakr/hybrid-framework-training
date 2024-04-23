23057c5bf5bfa65fe60341c40673981a549adbc8
Neerajakr <kr.neeraja25@gmail.com>
2024-04-23 00:44:57 -0700

    created package page and classes

--- src/test/java/com/allianz/test/LoginTest.java
+++ src/test/java/com/allianz/test/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.allianz.base.AutomationWrapper;
+import com.allianz.pages.LoginPage;
 import com.allianz.utils.DataUtils;
 
 public class LoginTest extends AutomationWrapper{
@@ -26,11 +27,16 @@ public class LoginTest extends AutomationWrapper{
 	public void validLoginTest(String username, String password, String header) {
 		
 		System.out.println("Login test ");
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+//		driver.findElement(By.name("username")).sendKeys(username);
+//		driver.findElement(By.name("password")).sendKeys(password);
+//		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
 
-		String actualHeader = driver.findElement(By.xpath("//h6[contains(normalize-space(),'Dash')]")).getText();
+//		String actualHeader = driver.findElement(By.xpath("//h6[contains(normalize-space(),'Dash')]")).getText();
+		String actualHeader =LoginPage.verifyDashboard(driver);
 		Assert.assertEquals(actualHeader,header);
 	}
 	
@@ -52,11 +58,18 @@ public class LoginTest extends AutomationWrapper{
 	@Test(dataProvider="commonDataProvider",dataProviderClass=DataUtils.class)
 	public void invalidLoginTest(String username, String password, String error) {
 		
-		System.out.println("Login test ");
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-	    String actualerror =driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+//		System.out.println("Login test ");
+//		driver.findElement(By.name("username")).sendKeys(username);
+//		driver.findElement(By.name("password")).sendKeys(password);
+//		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
+		
+//	    String actualerror =driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+	    String actualerror =LoginPage.verifyLoginError(driver);
+	    
 	    System.out.println("Login: "+actualerror);
 	    Assert.assertEquals(actualerror,error);
 	}
